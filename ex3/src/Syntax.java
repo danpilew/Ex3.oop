20a13bede383f26d166b82ba698f14f39dcb414a
ronen reshef <ronen4294@gmail.com>
2013-05-27 16:44:13 +0300

    syntax update

--- ex3/src/Syntax.java
+++ ex3/src/Syntax.java
@@ -1,15 +1,26 @@
 
 public class Syntax {
+	// VARIABELES
 	String varType = "[int, double, String, boolean, char]";
-	String premitionsOfMethods = "[public]"; // could be expanded
-	String methodReturn = "[void]"; // could be expanded
-	String Nspace = "\\s++"; // Necessary space, TO CHECK ABOUT POSSESIVE
-	String unNspace = "\\s++"; // unNecessary space,  TO CHECK ABOUT POSSESIVE
+	String varModifiers = "[final]";
+	String varName = "_?[\\d^].*"; // check it  
 	String intValue = "-?\\d";
 	String doubleValue = intValue +"(\\.d*)?"; // how to use point ( "." ) as point?
-	String charValue = ".";
+	String charValue = "."; // what about spaces?
 	String StringValue = " \" " + charValue + "*" +  " \" "; //  check IT
 	String booleanValue = "[true, false]" + "|" +  doubleValue;  // check IT
+	String Equality = "=";
+	
+	
+	//METHODS
+	String premitionsOfMethods = "[public]"; // could be expanded
+	String methodReturn = "[void]"; // could be expanded
+	
+	//GENERAL
+	String Nspace = "\\s++"; // Necessary space, TO CHECK ABOUT POSSESIVE
+	String unNspace = "\\s++"; // unNecessary space,  TO CHECK ABOUT POSSESIVE
+	
+	
 	
 	
 	
