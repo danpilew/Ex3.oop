9f156ab61eb199a6c25d5d9976ef0fb1e3e7e4d7
ronen reshef <ronen4294@gmail.com>
2013-05-28 09:40:19 +0300

    syntax

--- ex3/src/Syntax.java
+++ ex3/src/Syntax.java
@@ -1,38 +1,56 @@
 
 public class Syntax {
-	// VARIABELES
-	public final String varType = "[int, double, String, boolean, char]";
-	public final String varModifiers = "[final]";
-	public final String varName = "_?[\\d | \\s]^\\S*+"; // check it  
-	public final String intValue = "-?\\d";
-	public final String doubleValue = intValue +"(\\(.d+)?)?"; // how to use point ( "." ) as point?
-	public final String charValue = "."; // what about spaces?
-	public final String StringValue = " \" " + charValue + "*" +  " \" "; //  check IT
-	public final String booleanValue = "[true, false]" + "|" +  doubleValue;  // check IT
-	public final String any_Value = "["+ intValue + "," + doubleValue + "," + charValue + "," + 
-			StringValue + "," + booleanValue + "]";
-	public final String Equality = "=";
+	//GENERAL
+	public static final String Nspace = "\\s++"; // Necessary space, TO CHECK ABOUT POSSESIVE
+	public static final String unNspace = "\\s*+"; // unNecessary space,  TO CHECK ABOUT POSSESIVE
+	public static final String openBrackets = "(";
+	public static final String closeBrackets = ")";
+	public static final String openBlock = "{";
+	public static final String closeBlock = "}";
+	public static final String endLine = ";";
 	
+	// VARIABELES
+	public static final String varType = "[int, double, String, boolean, char]";
+	public static final String varModifiers = "[final]";
+	public static final String varName = "_?[\\d | \\s]^\\S*+"; // check it  
+	public static final String intValue = "-?\\d";
+	public static final String doubleValue = intValue +"(\\(.d+)?)?"; // how to use point ( "." ) as point?
+	public static final String charValue = "."; // what about spaces?
+	public static final String StringValue = " \" " + charValue + "*" +  " \" "; //  check IT
+	public static final String booleanValue = "[true, false]" + "|" +  doubleValue;  // check IT
+	public static final String any_Value = "["+ intValue + "," + doubleValue + "," + charValue + "," + 
+			StringValue + "," + booleanValue + "]";
+	public static final String Equality = "=";
 	
 	//METHODS
-	public final String premitionsOfMethods = "[public]"; // could be expanded
-	public final String methodReturn = "[void]"; // could be expanded
-	public final String methodName = "\\d\\S*+"; // check it
-	public final String openBrackets = "(";
-	public final String closeBrackets = ")";
+	public static final String premitionsOfMethods = "[public]"; // could be expanded
+	public static final String methodReturn = "[void]"; // could be expanded
+	public static final String methodName = "\\d\\S*+"; // check it
+	public static final String Return = "return";
+	
+	//IF, WHILE BLOCKS
+	public static final String IF = "if";
+	public static final String WHILE = "while";
+	public static final String AndOR = "&&" +"|"+ "||";
+	public static final String condition = +unNspace + openBrackets + (booleanValue + "|" + varName) +
+			(AndOR + unNspace +   ); // booleanValue included double
 	
 	
-	//GENERAL
-	public final String Nspace = "\\s++"; // Necessary space, TO CHECK ABOUT POSSESIVE
-	public final String unNspace = "\\s*+"; // unNecessary space,  TO CHECK ABOUT POSSESIVE
 	
 	// FINAL
-	public final String var_Line = unNspace + (varModifiers) + "?" + Nspace + (varType) + "?" + Nspace + 
-			(varName) + unNspace + Equality + "?" + (any_Value + "|" + varName) + "?"; // closes are OK?
+	public static final String var_Line = unNspace + (varModifiers) + "?" + Nspace + (varType) + "?" + Nspace + 
+			(varName) + unNspace + Equality + "?" + (any_Value + "|" + varName) + "?" + endLine; // closes are OK?
 			
-	public final String method_Line = unNspace + premitionsOfMethods + Nspace + methodReturn +
+	public static final String method_Defined_Line = unNspace + premitionsOfMethods + Nspace + methodReturn +
 			Nspace + methodName + unNspace + openBrackets + 
-			(varType + Nspace + varName) + "*" + closeBrackets; // check it
+			(varType + Nspace + varName) + "?" + unNspace + 
+			(","+ unNspace +varType + Nspace + varName+unNspace) + "*"
+			+ closeBrackets; // check it
+	
+	public static final String method_call_Line = unNspace + methodName + unNspace + openBrackets + 
+			varName + "?" + unNspace + (","+ unNspace + varName + unNspace) + "*" + 
+			closeBrackets + endLine;
+	
 
 	public Syntax() {
 		// TODO Auto-generated constructor stub
