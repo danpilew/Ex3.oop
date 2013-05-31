77faf320146f7dcd61b4fa6a14702c02413fa0dd
ronen reshef <ronen4294@gmail.com>
2013-06-01 00:59:06 +0300

    sss

---
+++ ex3/src/testSyntax.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import clids.ex4.compiler.Syntax;
+
+
+public class testSyntax {
+	public static void main(String[] args){
+		//String line = "int a = 1, b = 2, c = 3;";
+		String line = "int a = 1, b = 2;";
+		Pattern Var_PATTERN = Pattern.compile(Syntax.var_Line);
+		System.out.println(Var_PATTERN.toString());
+		Matcher VarMatcher = Var_PATTERN.matcher(line);
+		System.out.println(VarMatcher.matches());
+		
+		for (int i =0; i<= 8; i++)
+		System.out.println(i +"  " +VarMatcher.group(i));
+	}
+}
